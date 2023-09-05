3b8f56e899ea6de7427f38988fb1cf1b06eb910c
Viktor <vasha2020@gmail.com>
2023-09-05 21:18:10 +0100

    Animals are added

--- src/data/animalsImg.ts
+++ src/data/animalsImg.ts
@@ -1,3 +1,10 @@
 export const animalsImg = [
   {name: 'Cat', src: './images/animals/cat.jpeg' },
+  {name: 'Dog', src: './images/animals/dog.jpeg' },
+  {name: 'Elefant', src: './images/animals/elefant.jpeg' },
+  {name: 'Giraffe', src: './images/animals/giraffe.jpeg' },
+  {name: 'Monkey', src: './images/animals/monkey.jpeg' },
+  {name: 'Panda', src: './images/animals/panda.jpeg' },
+  {name: 'Rebbit', src: './images/animals/rebbit.jpeg' },
+  {name: 'Zebra', src: './images/animals/zebra.jpeg' },
 ];
